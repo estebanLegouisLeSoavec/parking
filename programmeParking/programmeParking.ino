01a95432984f855db17214e4a15cb5575bc71423
Esteban LEGOUIS <legouise@BTS-SN.LRQ>
2018-11-06 14:09:11 +0100

    programme principale

--- programmeParking/programmeParking.ino
+++ programmeParking/programmeParking.ino
@@ -1,7 +1,9 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
+#include <Wire.h>
 
+void setup() {
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
