8d1ebd88275245f5e040823ad9ec4ff7c2968c64
Esteban LEGOUIS <legouise@BTS-SN.LRQ>
2018-11-07 17:22:47 +0100

    programme de recupeeration des données des boucles

--- programmeParking/programmeParking.ino
+++ programmeParking/programmeParking.ino
@@ -1,11 +1,37 @@
+#include <EEPROM.h> // inclusion des librairies
 #include <LiquidCrystal.h>
 #include <Wire.h>
+#define Mecanisme 0x20 
+#define Auxiliaire 0x21
+#define Clavier 0x22
+#define Controleur_Lcd 0x3B
+#define Carte_Puce 0x50
+#define EEPROM_Maintenance 0x57 
+
+
+
 
 void setup() {
+    Serial.begin(9600); // initialisation de la liaison serie a 9600 bauds
+  Wire.begin(); 
+ // Wire.begin(Mecanisme); 
+  
 }
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
+char caractere;
+  Wire.requestFrom(Mecanisme,6); 
+  while(Wire.available())
+  {
+   caractere=Wire.read();
+   Serial.print(caractere);
+    
+  }
+  delay(500);
+//  Wire.endTransmission();
+
+
 
 }
