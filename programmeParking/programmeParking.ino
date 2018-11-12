c6aa0af3730ea82970fbd24a0ff58e1e6991ebed
Esteban LEGOUIS <legouise@BTS-SN.LRQ>
2018-11-12 10:44:29 +0100

    programme principal

--- programmeParking/programmeParking.ino
+++ programmeParking/programmeParking.ino
@@ -7,21 +7,44 @@
 #define Controleur_Lcd 0x3B
 #define Carte_Puce 0x50
 #define EEPROM_Maintenance 0x57 
+#include "I2C.h"
 
 
 
 
 void setup() {
-    Serial.begin(9600); // initialisation de la liaison serie a 9600 bauds
+   // Serial.begin(9600); // initialisation de la liaison serie a 9600 bauds
   Wire.begin(); 
  // Wire.begin(Mecanisme); 
-  
+ initI2C(100);
+ Wire.beginTransmission(Mecanisme);
+ Wire.write(0xFE);
+ Wire.endTransmission();
+ delay(5000);
+ Wire.beginTransmission(Mecanisme);
+ Wire.write(0xFD);
+ Wire.endTransmission(); 
+ delay(5000);
+  Wire.beginTransmission(Mecanisme);
+ Wire.write(0xFE);
+ Wire.endTransmission();
+ 
+
+
+
 }
 
 
 void loop() {
+/* Wire.beginTransmission(Mecanisme);
+ Wire.write(0xFD);
+ Wire.endTransmission();
+ delay(5000);
+ Wire.beginTransmission(Mecanisme);
+ Wire.write(0xFE);
+ Wire.endTransmission();
  
-char caractere;
+/*char caractere;
   Wire.requestFrom(Mecanisme,6); 
   while(Wire.available())
   {
@@ -31,7 +54,7 @@ char caractere;
   }
   delay(500);
 //  Wire.endTransmission();
-
+*/
 
 
 }
